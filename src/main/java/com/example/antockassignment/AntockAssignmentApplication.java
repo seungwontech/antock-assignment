c861bb139af02f03c42ff2fd7a6915625f70116c
seungwon <swlee173@gmail.com>
2025-03-25 21:00:45 +0900

    init

---
+++ src/main/java/com/example/antockassignment/AntockAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.antockassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntockAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntockAssignmentApplication.class, args);
+    }
+
+}
